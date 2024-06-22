80304a311bbc94d6fcac95f743b924d9980712c4
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-06-22 14:30:15 +0200

    refactor to prepare table driven decoding of more opcodes

--- decoder.go
+++ decoder.go
@@ -6,46 +6,60 @@ import (
 	"os"
 )
 
+func checkRead(n int, err error) {
+	if err != nil {
+		panic(err)
+	}
+	if n != 1 {
+		panic(fmt.Sprintf("wtf, not exactly %d bytes read", n))
+	}
+}
+
 func main() {
 	filePath := os.Args[1]
 
-	// Prpare the raw assembly insruction to decode
-	assembly, err := os.Open(filePath)
+	// Open file with assembly insructions to decode
+	file, err := os.Open(filePath)
 	if err != nil {
 		panic("fail to open file")
 	}
-	defer assembly.Close()
+	defer file.Close()
 
+	// Iterate over each instructiob, decoding them one by one
 	for {
-		if decodeInstruction(assembly) {
-			break
-		}
-	}
+		// We will parse instructions byte by byte
+		buffer := make([]byte, 1)
 
+		_, err := file.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		// Each instruction is at least 1 byte long.
+		// This byte contain the operation code.
+		// Each operation code require different parsing rule.
+		// Each operator can have multiple operation code.
+		opcode := buffer[0] >> 2 // Operation code
+		instruction := decoders[opcode](buffer, file)
+		fmt.Println(instruction)
+	}
 }
 
-func decodeInstruction(assembly *os.File) bool {
-	// b1 and b2 could be the same buffer but it's easier for debug
+// Register/memory to/from register
+func decode100010(buffer []byte, file *os.File) string {
+	const operator = "MOV"
+	d := buffer[0] & 2 // direction to/from register
+	w := buffer[0] & 1 // word/byte operator
 
-	// Parse byte 1
-	b1 := make([]byte, 1)
-	if checkRead(assembly.Read(b1)) != nil {
-		return true
-	}
+	// Parse second byte
+	checkRead(file.Read(buffer))
 
-	opcode := b1[0] >> 2 // Operation code
-	d := b1[0] & 2       // Direction to/from register
-	w := b1[0] & 1       // Word/byte operator
-
-	// Parse byte 2
-	b2 := make([]byte, 1)
-	if checkRead(assembly.Read(b2)) != nil {
-		return true
-	}
-
-	mod := b2[0] >> 6     // Register / memory mode
-	reg := b2[0] >> 3 & 7 // Register operand/extension of opcode
-	rm := b2[0] & 7       // Register operand/extension to use in EA calculation
+	mod := buffer[0] >> 6     // Register / memory mode
+	reg := buffer[0] >> 3 & 7 // Register operand/extension of opcode
+	rm := buffer[0] & 7       // Register operand/extension to use in EA calculation
 
 	// // Debug
 	// fmt.Printf("%b %b \n", b1[0], b2[0])
@@ -73,42 +87,35 @@ func decodeInstruction(assembly *os.File) bool {
 		operand2 = rm<<1 | w
 	}
 
-	fmt.Printf("%s %s, %s\n",
-		opcodeToInstruct[opcode],
+	return fmt.Sprintf("%s %s, %s",
+		operator,
 		registers[int8(operand1)],
 		registers[int8(operand2)],
 	)
-
-	return false
 }
 
-func checkRead(n int, err error) error {
-	if err != nil {
-		if err == io.EOF {
-			return err
-		}
-		panic("fuck")
-	}
-	if n == 0 {
-		panic("nope")
-	}
-	return nil
-}
-
-var opcodeToInstruct = map[uint8]string{
-	0b100010: "MOV",
+// Reference table 4-12 8086 Instruction Encoding
+var decoders = map[uint8]func([]byte, *os.File) string{
+	0b100010: decode100010, // MOV register/memory to/from register
+	// 0b110001: // MOV immediate to register/memory
+	// 0b101100: // MOV immediate to register
+	// 0b101101: // MOV immediate to register
+	// 0b101110: // MOV immediate to register
+	// 0b101111: // MOV immediate to register
+	// 0b101000: // MOV memory to accumulator / Accumulator to memory
+	// 0b100011: // MOV megister/memory to segment register / Segment register to register/memory
 }
 
 // Reference Table 4-8 MOD(Mode) Field Encoding
-var modeToLabel = map[int8]string{
-	0b00: "Memory Mode, no displacement",
-	0b01: "Memory Mode, 8-bit displacement",
-	0b10: "Memory Mode, 16-bit displacement",
-	0b11: "Register Mode, no displacement",
-}
+// var modeToLabel = map[int8]string{
+// 	0b00: "Memory Mode, no displacement",
+// 	0b01: "Memory Mode, 8-bit displacement",
+// 	0b10: "Memory Mode, 16-bit displacement",
+// 	0b11: "Register Mode, no displacement",
+// }
 
 // Reference Table 4-9 REG(Register) Encoding
-// This table take 4 bits. The 3 from REG fist then the one from W
+// Fist 3 bits come from REG and last one from W
 var registers = map[int8]string{
 	0b0000: "AL",
 	0b0010: "CL",
