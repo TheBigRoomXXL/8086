b95d004a7fddbf3402e54167e7ece7ee635b6146
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-07-07 10:59:49 +0200

    fix: getting 1 byte instead of 2 when w=1

--- decode.go
+++ decode.go
@@ -16,6 +16,7 @@ type ReaderCounter struct {
 
 func (r *ReaderCounter) Read(p []byte) (int, error) {
 	n, err := r.reader.Read(p)
+	// fmt.Printf("%08b ", p) // Great for debugging
 	r.count += n
 	return n, err
 }
@@ -228,7 +229,7 @@ func decodeImediateToRegMem(buffer []byte, bus *ReaderCounter) Instruction {
 	} else if s == 0 && w == 1 {
 		operand2 = fmt.Sprintf("%d", uint16(getData16(bus)))
 	} else if s == 1 && w == 1 {
-		operand2 = fmt.Sprintf("%d", getData8(bus))
+		operand2 = fmt.Sprintf("%d", getData16(bus))
 	} else {
 		panic("should not happen")
 	}
